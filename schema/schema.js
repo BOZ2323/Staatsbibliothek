4f0c760d7a76563b9a1077e1eac9ba1fef22164c
Lilli Baaske <baaske@architrave.de>
2020-09-30 12:17:41 +0200

    Create rootQuery

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,24 @@
 const graphql = require('graphql')
+const lodash = require('lodash')
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+/*
+// dummy data
+const books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+    { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+    { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+    { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
+];
+const authors = [
+    { name: 'Patrick Rothfuss', age: 44, id: '1' },
+    { name: 'Brandon Sanderson', age: 42, id: '2' },
+    { name: 'Terry Pratchett', age: 66, id: '3' }
+];
+*/
 
 const BookType = new GraphQLObjectType({
     name : 'Book',
@@ -9,4 +27,22 @@ const BookType = new GraphQLObjectType({
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
     })
+})
+const RootQuery  = new GraphQLObjectType({
+    name : 'RootQueryType',
+    fields: () => ({
+        book: {
+            type: BookType,
+            args: {id: GraphQLString},
+            resolve(parent,args){
+                //code to get data from db or other source
+                return_find(books,{id: args.id})
+            },
+            
+        }
+    })
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
